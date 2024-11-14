f08fe9ef41c2548a8f028dee686530022e1c9e2f
patricio11-loki <61088358+patricio11-loki@users.noreply.github.com>
2024-11-14 14:36:55 -0500

    .

--- script1.js
+++ script1.js
@@ -4,3 +4,5 @@ for (let i = 0; i < numeros.length; i++){
     sumaTotal += numeros[i];
 }
 document.write("La suma total es", sumaTotal);
+let promedio=sumaTotal/numeros.length;
+document.write("El promedio es:", promedio);

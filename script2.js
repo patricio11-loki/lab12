82f70219d921ae43bed4b1bd15d7d122153ef843
patricio11-loki <61088358+patricio11-loki@users.noreply.github.com>
2024-11-14 11:38:34 -0500

    .

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-let numero=0;
-while (numero>0){
+let numero;
+while (true){
     numero = parseFloat(prompt("Ingresa un número , Ingresa un número negativo para salir"))
     if (numero>=0){
         document.write("Has Ingresado", numero);

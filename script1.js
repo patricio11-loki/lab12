dad951c2e618fae9edfca9fe1a0a41e75fa00589
patricio11-loki <61088358+patricio11-loki@users.noreply.github.com>
2024-11-14 11:25:33 -0500

    nuevo sc

--- script1.js
+++ script1.js
@@ -1,10 +1,9 @@
 let numero=0;
-while(numero>0){
-    numero = parseFloat(prompt("Ingresa un número (ingresa un número negativo para salir):"))
-if (numero>=0){
-    document.write("Has Ingresado:", numero);
-}else{
-    document.write{"número negativo ingresado, saliendo del bucle";
+while (numero>0){
+    numero = parseFloat(prompt("Ingresa un número , Ingresa un número negativo para salir"))
+    if (numero>=0){
+        document.write("Has Ingresado", numero);
+    }else{
+        document.write("Número negativo ingresado, salñiedo del bucle")
     }
 }
-} 
